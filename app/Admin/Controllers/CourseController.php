fa55001486bfa84d53380350c9e31b39f0895870
Janith Niroshan <129001487+neroIJN@users.noreply.github.com>
2024-11-14 01:20:26 +0530

    Unitl 2.07

--- app/Admin/Controllers/CourseController.php
+++ app/Admin/Controllers/CourseController.php
@@ -46,14 +46,35 @@ class CourseController extends AdminController
     {
         $form = new Form(new Course());
         $form->text('name', __('Name'));
+        //get our catogaries
+        //key value pair
+        //last one  is the key 
         $result = CourseType::pluck('title', 'id');
-        dd($result);
-        $form->select('parent_id', __('Parent Category'))->options((new CourseType())::selectOptions());
-        $form->text('title', __('Title'));
-        $form->textarea('description', __('Description'));
-        $form->number('order', __('Order'));    
+        //select method helps you select one of the options the comes from result
+        //dd($result);
+        $form->select('type_id', __('Category'))->options($result);
+        $form->image('thumbnail', __('Thumbnail'))->uniqueName();
+        //File is used for video and other format like pdf/doc
+        $form->file('video', __('Video'))->uniqueName();
+        $form->text('description', __('Description'));
+        //decimal method helps with retrieving float format from the database
+        $form->decimal('price', __('Price'));
+        $form->number('lesson_num', __('Lesson Number'));
+        $form->number('video_length', __('Video Length'));
+        //for posting, who is posting
+        $result = User::pluck('name','token');
+        $form->select('user_token',__('Teacher'))->options($result);
+        $form->display('created_at', __('Created at'));
+        $form->display('updated_at', __('Updated at'));
 
         return $form;
+        // dd($result);
+        // $form->select('parent_id', __('Parent Category'))->options((new CourseType())::selectOptions());
+    
+        // $form->text('title', __('Title'));
+        // $form->textarea('description', __('Description'));
+        // $form->number('order', __('Order'));    
+
     }
 
 }
