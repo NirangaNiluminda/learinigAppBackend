64f9d32bcb88add75d9618807663a36a5db464f9
NirangaNiluminda <niranganiluminda@gmail.com>
2024-12-02 19:54:47 +0530

    resolve the error

--- routes/api.php
+++ routes/api.php
@@ -8,20 +8,22 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
-Route::group(['namespace' => 'Api'], function () {
+// Route::group(['namespace' => 'Api'], function () {
 
-    // Only one login route, pointing to the correct method
-    Route::post('/login', [UserController::class, 'login']);
-    //Route::post('/login','UserController@createUser');
+//     // Only one login route, pointing to the correct method
+//     Route::post('/login', [UserController::class, 'login']);
+//     //Route::post('/login','UserController@createUser');
 
-    // Authentication middleware group using Sanctum
-    Route::group(['middleware' => ['auth:sanctum']], function () {
-        Route::any('courseList', [CourseController::class, 'courseList']);
-    });
-});
+//     // Authentication middleware group using Sanctum
+//     Route::group(['middleware' => ['auth:sanctum']], function () {
+//         Route::any('/courseList', [CourseController::class, 'courseList']);
+//     });
+// });
 
    
+Route::post('/login', [UserController::class, 'login']);
 
+Route::any('/courseList', [CourseController::class, 'courseList']);
 
 
 Route::get('/student', function () {
